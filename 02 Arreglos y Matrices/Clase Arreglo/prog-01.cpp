d6b6a215c7d0f128cb4a9f94c00e896f61b43dc9
Gerado Beltran <contacto@gerardobeltran.mx>
2023-02-26 00:09:59 -0700

    --

--- 02 Arreglos y Matrices/Clase Arreglo/prog-01.cpp
+++ 02 Arreglos y Matrices/Clase Arreglo/prog-01.cpp
@@ -29,6 +29,54 @@ class Arreglo {
     int ObtenerNumeroElementos (){
         return ultimo + 1;
     }
+
+    int ObtenerElementoMenor(){
+        int menor = ObtenerElemento(0);
+          
+        for (int i=1; i < ObtenerNumeroElementos(); i++){
+            if (menor > ObtenerElemento(i)){
+                menor = ObtenerElemento(i);
+            }
+        }
+        return menor;
+       }
+
+     int ObtenerElementoMayor(){
+        int mayor = ObtenerElemento(0);
+          
+        for (int i=1; i < ObtenerNumeroElementos(); i++){
+            if (mayor < ObtenerElemento(i)){
+                mayor = ObtenerElemento(i);
+            }
+        }
+        return mayor;
+       }
+       
+int ObtenerElementoPosicion(int dato){
+        int mayor = ObtenerElemento(0);
+        int posicion=-1; 
+        for (int i=0; i < ObtenerNumeroElementos() and posicion==-1; i++){
+            if (dato == ObtenerElemento(i)){
+                posicion=i;
+            }
+        }
+        return posicion;
+       }
+     
+
+
+       int ObtenerSumatoria(){
+        int suma =0;
+        for (int i=0; i < ObtenerNumeroElementos(); i++){
+            suma += ObtenerElemento(i);
+        }
+        return suma;
+       }
+       
+       double ObtenerPromedio (){
+           return (double) ObtenerSumatoria() / ObtenerNumeroElementos();
+       }
+
 };
 
 int main()
@@ -43,6 +91,11 @@ int main()
     
     cout << edades.ObtenerNumeroElementos() << endl;
     cout << edades.ObtenerLongitudMaxima() << endl;
+    cout << edades.ObtenerSumatoria() << endl;
+    cout << edades.ObtenerPromedio() << endl;
+    cout << edades.ObtenerElementoMenor() << endl;
+    cout << edades.ObtenerElementoMayor() << endl;
+    cout << edades.ObtenerElementoPosicion(43) << endl;
     
     for (int i=0; i < edades.ObtenerNumeroElementos(); i++){
         cout << edades.ObtenerElemento(i) << " ";
