b7aacaf734d4e0b17055a5092e47dff4119c4ce3
Gerardo Beltrán <contacto@gerardobeltran.mx>
2023-03-03 11:48:41 -0700

    --

---
+++ 02 Arreglos y Matrices/B. Canicas/prog-01.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+
+
+using namespace std;
+
+template <class T>
+class Arreglo {
+    private: 
+        T *datos;
+        int ultimo;
+        int longitud;
+    public:
+    Arreglo (int tam){
+        datos = new T[tam];
+        longitud = tam;
+    } 
+    ~Arreglo(){
+        delete [] datos;
+    }
+    Arreglo(){
+        ultimo = -1;
+        datos = new T[100];
+        longitud = 100;
+        
+    }
+    void AgregarElemento(T dato){
+        ultimo++;
+        datos[ultimo] = dato;
+    }  
+    
+    T ObtenerElemento (int pos){
+        return datos[pos];
+    }
+    void CambiarElemento (int pos, T nuevoDato){
+        datos[pos] = nuevoDato;
+    }
+    int ObtenerLongitudMaxima(){
+        return longitud;
+    } 
+    int ObtenerNumeroElementos (){
+        return ultimo + 1;
+    }
+
+    T ObtenerElementoMenor(){
+        T menor = ObtenerElemento(0);
+          
+        for (int i=1; i < ObtenerNumeroElementos(); i++){
+            if (menor > ObtenerElemento(i)){
+                menor = ObtenerElemento(i);
+            }
+        }
+        return menor;
+       }
+
+    T ObtenerElementoMayor(){
+        T mayor = ObtenerElemento(0);
+          
+        for (int i=1; i < ObtenerNumeroElementos(); i++){
+            if (mayor < ObtenerElemento(i)){
+                mayor = ObtenerElemento(i);
+            }
+        }
+        return mayor;
+       }
+       
+    int ObtenerElementoPosicion(T dato){
+        int posicion=-1; 
+        for (int i=0; i < ObtenerNumeroElementos() and posicion==-1; i++){
+            if (dato == ObtenerElemento(i)){
+                posicion=i;
+            }
+        }
+        return posicion;
+    }
+     
+
+
+    T ObtenerSumatoria(){
+        T suma =0;
+        for (int i=0; i < ObtenerNumeroElementos(); i++){
+            suma += ObtenerElemento(i);
+        }
+        return suma;
+    }
+       
+    double ObtenerPromedio (){
+           return (double) ObtenerSumatoria() / ObtenerNumeroElementos();
+    }
+
+};
+
+int main()int
+{
+    
+    int posicion, N, C;
+    //cout << "Cantidad de vasos y canicas: ";
+    cin >> N >> C;
+    
+    Arreglo <int> vasos(N);
+
+    for (int i=0; i< N; i++){
+        vasos.AgregarElemento(0);
+    }
+    
+    for (int i=0; i < C; i++ ){
+        cin >> posicion;
+        vasos.CambiarElemento(posicion-1,
+                vasos.ObtenerElemento(posicion-1) + 1);
+    }
+
+    for (int i=0; i<vasos.ObtenerNumeroElementos(); i++){
+        cout << vasos.ObtenerElemento(i) << endl;
+    }
+
+    return 0;
+}
