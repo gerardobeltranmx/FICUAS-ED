330e8ccce2c00cc3b9d09666f15f3bd83b550363
Gerardo Beltrán <contacto@gerardobeltran.mx>
2023-03-27 11:57:20 -0700

    --

--- 03 Pilas/00 Clase Pila/prog.cpp
+++ 03 Pilas/00 Clase Pila/prog.cpp
@@ -1,51 +1,78 @@
 #include  <iostream>
+using namespace std;
 class TPila{
-
     int tam;
     int tope;
     int *elementos;
 
-    /Iniciar Pila
-    void iniciarPila(int N){
-        elementos= 	(int*)malloc(sizeof(int)*N);
-        tam=N;
-	    tope=-1;
+    //Iniciar Pila
+    public:
+    TPila(int N){
+      elementos=(int*)malloc(sizeof(int)*N);
+      tam=N;
+	  tope=-1;
 }
-// Agregar un dato a la Pila
-void agregarPila(int dato){
-	pila->tope++;
-	pila->elementos[pila->tope]=dato;
-}
+    // Agregar un dato a la Pila
+    void agregarPila(int dato){
+        tope++;
+        elementos[tope]=dato;
+    }
 
-int extraerPila(){
-	int dato;
-	dato = elementos[pila->tope];
-	tope--;
-	return dato;
-}
-//Obtener dato del tope sin extraerlo
-int obtenerElementoTopePila(){
-	return elementos[pila.tope];
-}
+    int extraerPila(){
+        int dato;
+        dato = elementos[tope];
+        tope--;
+        return dato;
+    }
+    //Obtener dato del tope sin extraerlo
+    int obtenerElementoTopePila(){
+        return elementos[tope];
+    }
 
-/Determina si la Pila esta vacía
-bool vaciaPila(){
-	return(tope==-1)?true:false;
-}
+    //Determina si la Pila esta vacía
+    bool vaciaPila(){
+        return(tope==-1)?true:false;
+    }
 
-//Determina si la Pila esta llena
-bool llenaPila(Tpilapila){
-	return (tope==pila.tam-1)?true:false;
-}
+    //Determina si la Pila esta llena
+    bool llenaPila(){
+        return (tope==tam-1)?true:false;
+    }
 
-//Obtiene numero de elementos de la Pila
-int numElementosPila(){
-	return tope+1;
-}
-//Obtiene un elemento de la Pila de alguna posición 
-int obtenerElementoPila(int pos){
-	return elementos[pos];
-}
+    //Obtiene numero de elementos de la Pila
+    int numElementosPila(){
+        return tope+1;
+    }
+    //Obtiene un elemento de la Pila de alguna posición 
+    int obtenerElementoPila(int pos){
+        return elementos[pos];
+    }
+};
+
+int main(){
+
+    TPila miPila (10);
+
+
+    miPila.agregarPila(7);
+    miPila.agregarPila(9);
+    miPila.agregarPila(1);
+    miPila.agregarPila(4);
+
+    cout << miPila.vaciaPila() << endl;
+
+    std::cout << miPila.extraerPila() << std::endl;
+    std::cout << miPila.extraerPila() << std::endl;
+    std::cout << miPila.extraerPila() << std::endl;
+    std::cout << miPila.extraerPila() << std::endl;
+
+    cout << miPila.vaciaPila() << endl;
+
+
+
+    
+
+
 
 
 }
