f41c06b0a071e69f5245f17cdd0165bee2bde141
Gerado Beltran <contacto@gerardobeltran.mx>
2023-03-28 00:51:35 -0700

    --

--- 03 Pilas/00 Clase Pila/prog.cpp
+++ 03 Pilas/00 Clase Pila/prog.cpp
@@ -1,72 +1,74 @@
 #include  <iostream>
 using namespace std;
-class TPila{
+
+template <class T>
+class Pila{
     int tam;
     int tope;
-    int *elementos;
+    T *elementos;
 
     //Iniciar Pila
     public:
-    TPila(int N){
-      elementos=(int*)malloc(sizeof(int)*N);
+    Pila(int N){
+      elementos= new T[N];
       tam=N;
 	  tope=-1;
-}
+    }       
     // Agregar un dato a la Pila
-    void agregarPila(int dato){
+    void agregar(int dato){
         tope++;
         elementos[tope]=dato;
     }
-
-    int extraerPila(){
+    // extraer un dato de la pila
+    T extraer(){
         int dato;
         dato = elementos[tope];
         tope--;
         return dato;
     }
     //Obtener dato del tope sin extraerlo
-    int obtenerElementoTopePila(){
+    T cima(){
         return elementos[tope];
     }
 
     //Determina si la Pila esta vacía
-    bool vaciaPila(){
+    bool esVacia(){
         return(tope==-1)?true:false;
     }
 
     //Determina si la Pila esta llena
-    bool llenaPila(){
+    bool esLlena(){
         return (tope==tam-1)?true:false;
     }
 
     //Obtiene numero de elementos de la Pila
-    int numElementosPila(){
+    int numElementos(){
         return tope+1;
     }
     //Obtiene un elemento de la Pila de alguna posición 
-    int obtenerElementoPila(int pos){
+    T obtenerPorPosicion(int pos){
         return elementos[pos];
     }
 };
 
 int main(){
 
-    TPila miPila (10);
+    Pila<int> miPila (10);
 
 
-    miPila.agregarPila(7);
-    miPila.agregarPila(9);
-    miPila.agregarPila(1);
-    miPila.agregarPila(4);
+    miPila.agregar(7);
+    miPila.agregar(9);
+    miPila.agregar(1);
+    miPila.agregar(4);
 
-    cout << miPila.vaciaPila() << endl;
+    cout << miPila.esVacia() << endl;
 
-    std::cout << miPila.extraerPila() << std::endl;
-    std::cout << miPila.extraerPila() << std::endl;
-    std::cout << miPila.extraerPila() << std::endl;
-    std::cout << miPila.extraerPila() << std::endl;
+    std::cout << miPila.extraer() << std::endl;
+    std::cout << miPila.extraer() << std::endl;
+    std::cout << miPila.extraer() << std::endl;
+    std::cout << miPila.extraer() << std::endl;
 
-    cout << miPila.vaciaPila() << endl;
+    cout << miPila.esVacia() << endl;
 
 
 
