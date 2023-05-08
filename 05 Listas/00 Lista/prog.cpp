9dea7fe64107efe4218a26e1352d415a3312463a
Gerardo Beltrán <contacto@gerardobeltran.mx>
2023-05-08 11:41:24 -0700

    --

--- 05 Listas/00 Lista/prog.cpp
+++ 05 Listas/00 Lista/prog.cpp
@@ -4,14 +4,13 @@ using namespace std;
 
 template <class T>
 class Lista {
-
     class Nodo{
+       public:
         T info;
         Nodo *enl;
     };
-
     Nodo *Raiz;
-
+    public:
     Lista(){
         Raiz = NULL;
     }
@@ -35,7 +34,40 @@ class Lista {
         delete aux;
         return dato;
     }
-}
+    void agregarFinal(T dato){
+
+       if (Raiz==NULL){
+            agregarInicio(dato);
+       }
+       else{
+          Nodo *aux = Raiz;   
+          while (aux->enl!=NULL){
+            aux=aux->enl;
+          }
+          // crea nuevo nodo
+          Nodo *nuevo = new Nodo;
+          nuevo->info = dato;
+          nuevo->enl = NULL;
+          // colocamos al final 
+          aux->enl = nuevo;
+       }     
+
+
+
+    }
+
+
+    void imprimir(){
+        Nodo *aux;
+        aux = Raiz;
+
+        while (aux!=NULL){
+            cout << aux->info << " ";
+            aux = aux->enl;
+        }
+        cout << endl;
+    }
+};
 
 int main()
 {
@@ -45,7 +77,20 @@ int main()
     miLista.agregarInicio(6);
     miLista.agregarInicio(8);
     miLista.agregarInicio(7);
+    miLista.agregarInicio(1);
+    miLista.agregarInicio(5);
+    miLista.agregarFinal(20);
+    miLista.agregarFinal(30);
+    miLista.agregarFinal(40);
+    miLista.agregarInicio(54);
     
+    miLista.imprimir();
+    cout << "Se elimino el: " << miLista.eliminarInicio()<< endl;
+    miLista.imprimir();
+    cout << "Se elimino el: " << miLista.eliminarInicio()<< endl;
+    miLista.imprimir();
+
+        
     
 
     return 0;
