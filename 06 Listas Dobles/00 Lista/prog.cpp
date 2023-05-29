9d38b65939a8e36ec487dfaad9e8ee49b948f27a
Gerardo Beltrán <contacto@gerardobeltran.mx>
2023-05-29 11:54:16 -0700

    --

--- 06 Listas Dobles/00 Lista/prog.cpp
+++ 06 Listas Dobles/00 Lista/prog.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 template <class T>
-class ListaDoble {
+class ListaDoble
+{
     class Nodo{
        public:
         T info;
@@ -76,134 +77,30 @@ class ListaDoble {
         }
         tam=0;
     }
-    
-       /*  T eliminarInicio(){
-        T dato;
-        Nodo *aux;
-        aux = Raiz;
-        Raiz = Raiz->enl;
-        dato = aux->info;
-        delete aux;
-        tam--;
-        return dato;
-    }
-    void agregarFinal(T dato){
 
-       if (Raiz==NULL){
-            agregarInicio(dato);
-       }
-       else{
-          Nodo *aux = Raiz;   
-          while (aux->enl!=NULL){
-            aux=aux->enl;
-          }
-          // crea nuevo nodo
-          Nodo *nuevo = new Nodo;
-          nuevo->info = dato;
-          nuevo->enl = NULL;
-          // colocamos al final 
-          aux->enl = nuevo;
-          tam++; 
-
-       }    
-    }
-    T eliminarFinal(){
-        Nodo *act, *sig;
-        T dato;
-        act = Raiz;
-        sig = act->enl;
-
-        if (sig==NULL){ // solo un nodo
-            eliminarInicio();
-        }
-        else if (sig->enl == NULL){ // solo 2 nodos
-            dato = sig->info;
-            delete sig;
-            act->enl =NULL; 
-            tam--;
-        }
-        else { // 3 o mas nodos
-            while (sig->enl != NULL){
-                act = sig;
-                sig = sig->enl;
-            }
-            dato = sig->info;
-            delete sig;
-            act->enl =NULL;
-            tam--;
-        }
-        return dato;
-    }
-
-    void eliminarTodos(){
-        Nodo *aux;
-        while (Raiz!=NULL){
-            aux = Raiz;
-            Raiz = Raiz->enl;
-            delete aux;
-        }
-        tam=0;
-    }
-    void imprimir(){
-        Nodo *aux;
-        aux = Raiz;
-
+    void invertir(){
+        Nodo *temp = NULL;
+        Nodo *aux = Raiz;
         while (aux!=NULL){
-            cout << aux->info << " ";
-            aux = aux->enl;
+           Nodo *nuevo;
+            // crear nuevo nodo
+            nuevo = new Nodo;
+            nuevo->info = aux->info;
+            nuevo->ant = NULL;
+            nuevo->sig = NULL;
+
+            nuevo->sig= temp; // Coloca nodo al inicio de lista
+           if ( temp!=NULL) // Si no esta vacía
+                 temp->ant=nuevo; //Hacer referencia al nuevo primer
+   
+            temp = nuevo; // El inicio de lista cambia al nuevo
+            aux = aux->sig;
         }
-        cout << endl;
+        eliminarTodos();
+        Raiz = temp;
     }
-    int obtenerNumElementos(){
-        return tam;
-    }
-
-    bool esVacia(){
-        //return tam == 0?true:false;
-          return Raiz==NULL?true:false;  
-    }
-
-    bool buscarPorIndice(int indice, T &dato){
-        Nodo *aux = Raiz;
-        int cont=0;
-        bool enc=false;
-
-        for (int i=0; i<obtenerNumElementos() and enc==false; i++ ){
-                if (i==indice){
-                    enc = true;
-                    dato = aux->info;
-                }    
-                else
-                    aux = aux->enl;    
-        }
-
-        return enc; 
-
-
-        
-
-    }
-
-
-
-    int buscarPorValor(T valor){
-        Nodo *aux = Raiz;
-        bool enc = false;
-        int cont=0;
-        while (aux!=NULL and enc == false){
-            if (aux->info == valor){
-               enc = true;     
-            }
-            else{
-                aux= aux->enl;
-                cont++;
-            }
-        }
-        return enc==true?cont:-1;
-    }
-
-
- */
+    
+      
 };
 
 int main()
@@ -217,8 +114,10 @@ int main()
     miLista.agregarInicio(1);
     miLista.agregarInicio(5);
     miLista.imprimir();
-
-
+    miLista.invertir();
+    cout << "nueva lista "<< endl;
+    miLista.imprimir();
+    
     return 0;
 
    /*  miLista.agregarFinal(20);
